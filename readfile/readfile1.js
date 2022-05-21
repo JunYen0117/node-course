7ea9cc9fbd4682e15fc213a77256187c39fae849
JunYen0117 <relife12345@gmail.com>
2022-05-21 14:43:31 +0800

    add files

--- readfile/readfile1.js
+++ readfile/readfile1.js
@@ -1,35 +1,42 @@
-// const fs = require('fs');
+const fs = require('fs');
 
-// fs.readFile('test11111.txt', 'utf-8', (err, data) => {
-//   if (err) {
-//     // 錯誤了
-//     console.log('喔喔喔，發生錯誤了');
-//     console.error(err);
-//   } else {
-//     // 因為沒有 err，所以是正確的
-//     console.log(data);
-//   }
-// });
-
-let a = new Promise((resolve, reject) => {
-    const fs = require('fs');
-    fs.readFile('test11.txt', 'utf-8', (err, data)=> {
-        if (err) {
-          // 錯誤了
-          reject(err);
-        //   console.log('喔喔喔，發生錯誤了');
-        //   console.error(err);
-        } else {
-          // 因為沒有 err，所以是正確的
-          resolve(data);
-        //   console.log(data);
-        }
-      });
+fs.readFile('test11111.txt', 'utf-8', (err, data) => {
+  if (err) {
+    // 錯誤了
+    console.log('喔喔喔，發生錯誤了');
+    console.error(err);
+  } else {
+    // 因為沒有 err，所以是正確的
+    console.log(data);
+  }
 });
 
-a.then((success)=>{
-    console.log(success);
-})
-.catch((error)=>{
-    console.log('喔喔喔，發生錯誤了');
-})
+
+async function main () {
+
+}
+
+try {
+  
+} catch (error) {
+  
+}
+
+// let a = new Promise((resolve, reject) => {
+//     const fs = require('fs');
+//     fs.readFile('test.txt', 'utf-8', (err, data)=> {
+//         if (err) {
+//           reject(err);
+//         } else {
+//           resolve(data);
+//         }
+//       });
+// });
+
+// a.then((success)=>{
+//     console.log(success);
+// })
+// .catch((error)=>{
+//     console.log('喔喔喔，發生錯誤了');
+// })
+
